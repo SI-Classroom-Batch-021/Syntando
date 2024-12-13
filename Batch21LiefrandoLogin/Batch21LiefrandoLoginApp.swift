15206d3958e6121a310ffd1a2c9d6ef8d26472c5
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2024-12-13 08:52:06 +0100

    Initial Commit

---
+++ Batch21LiefrandoLogin/Batch21LiefrandoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Batch21LiefrandoLoginApp.swift
+//  Batch21LiefrandoLogin
+//
+//  Created by Till Hemmerich on 13.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Batch21LiefrandoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Batch21LiefrandoLoginTests/Batch21LiefrandoLoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  Batch21LiefrandoLoginTests.swift
+//  Batch21LiefrandoLoginTests
+//
+//  Created by Till Hemmerich on 13.12.24.
+//
+
+import Testing
+@testable import Batch21LiefrandoLogin
+
+struct Batch21LiefrandoLoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
